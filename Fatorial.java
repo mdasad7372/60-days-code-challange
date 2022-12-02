e815421ae1724c0010c297ce7d10e96c182e445c
mdasad7372 <119691015+mdasad7372@users.noreply.github.com>
2022-12-02 23:32:39 +0530

    Fatorial.java
    1 days completed

---
+++ Fatorial.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+public class Fatorial 
+{ 
+public static void main(String []args) 
+{ 
+Scanner sc=new Scanner(System.in); 
+System.out.println("Enter the number: "); 
+int num=sc.nextInt(); 
+int i=1,fact=1; 
+while(i<=num) 
+{ 
+fact=fact*i; 
+i++; 
+} 
+System.out.println("Factorial of the number: "+fact); 
+} 
+}
+
